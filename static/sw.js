49aac75057c6012d2d72bf4c29d338decd4b5968
revendacontrole-wq <revendacontrole@gmail.com>
2026-09-04 16:40:08 -0300

    Remove RV Maxima reference and bump cache to v11: static/sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,22 +1,20 @@
-const CACHE_NAME = 'jmb-perf-v1';
-const ASSETS = [
-  '/',
-  '/static/index.html',
-  '/static/css/style.css',
-  '/static/js/app.js'
-];
+const CACHE_NAME = 'jmb-perf-v11';
 
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => caches.delete(key))
+    )).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  if (e.request.url.includes('/api/')) {
-    return fetch(e.request);
-  }
+  // Always fetch fresh network requests for index.html and static files
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
